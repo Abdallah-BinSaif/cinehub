26b6bc4a6703a4b3f00bf95c73ff6b1b7458de33
Abdallah-BinSaif <binsaif.info@gmail.com>
2024-12-04 19:58:26 +0600

    form setup created

--- src/pages/AddMovie.jsx
+++ src/pages/AddMovie.jsx
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useForm } from "react-hook-form";
+import { Rating } from 'react-simple-star-rating'
 
 const AddMovie = () => {
+    const [rating, setRating] = useState(0)
     const {
         register,
         handleSubmit,
         formState:{errors},
-        watch} = useForm()
+        watch} = useForm({
+        // defaultValues:{
+        //     rate: rating,
+        // }
+    })
 
-    console.log(errors)
     const years = [2024, 2023, 2022,2021, 2020, 2019,2018]
+    const handleRating = (rate) => {
 
+        setRating(rate)
+        // other logic
+    }
 
     return (
         <div>
             <form
                 onSubmit={handleSubmit((data) => {
-                    console.log(data)
-                })} className={"p-8 bg-third *:w-full"}>
+                    console.log({...data, rating})
+                })}
+                className={"p-8 bg-third *:w-full"}>
                 {/*Input row*/}
                 <div className={"flex gap-4"}>
                     <div className={"w-1/2"}>
@@ -46,12 +56,13 @@ const AddMovie = () => {
                         /><br/>
                         <p className={"text-red-500"}>{errors.name?.message}</p>
                     </div>
-                </div> <br/>
+                </div>
+                <br/>
 
                 {/*Input row*/}
                 <div className={"flex gap-4"}>
                     <select className={"w-1/3"} {...register("genre")}>
-                        <option disabled={"ture"} value={""}>Select Genre</option>
+                        <option disabled={true} value={""}>Select Genre</option>
                         <option value={"comedy"}>Comedy</option>
                         <option value={"drama"}>Drama</option>
                         <option value={"horror"}>Horror</option>
@@ -73,51 +84,37 @@ const AddMovie = () => {
                         <p className={"text-red-500"}>{errors.duration?.message}</p>
                     </div>
                     <select className={"w-1/3"} {...register("year")}>
-                        <option disabled={"true"} value="">Select a Year</option>
+                        <option disabled={true} value="">Select a Year</option>
                         {
-                            years.map(year => <option value={`${year}`}>{year}</option>)
+                            years.map((year, idx) => <option key={idx} value={`${year}`}>{year}</option>)
                         }
                     </select>
 
                 </div>
                 <br/>
-
-                {/*Input row*/}
-                <div className={"flex gap-4"}>
-                    <div className={"w-1/2"}>
-                        <input
-                            {...register("name", {
-                                required: "this is required",
-                                minLength: {
-                                    value: 3,
-                                    message: "Min length is 3"
-                                }
-                            })}
-                            placeholder={"Name"}
-                            className={"w-full input"}
-                        /><br/>
-                        <p className={"text-red-500"}>{errors.name?.message}</p>
-                    </div>
-                    <div className={"w-1/2"}>
-                        <input
-                            {...register("email", {
-                                required: "Please Enter an Email address",
-                            })}
-                            type={"email"}
-                            placeholder={"Email"}
-                            className={"w-full input"}
-                        /><br/>
-                        <p className={"text-red-500"}>{errors.email?.message}</p>
-                    </div>
+                <div className={"flex flex-col justify-center items-center"}>
+                    <label>Rate this Movie</label>
+                    <Rating
+                        onClick={handleRating}
+                    ></Rating>
                 </div>
+                <br/> <br/>
+                <div>
+                    <textarea
+                        {...register("summary", {
+                            required: "this is required",
+                            minLength: {
+                                value: 10,
+                                message: "minimum 10 word allowed"
+                            }
+                        })}
+                        rows={5}
+                        placeholder={"write the summary of the movie"}
+                        className={"w-full"}
+                    /><br/>
+                    <p className={"text-red-500"}>{errors.summary?.message}</p>
+                </div><br/>
 
-                {/*<input*/}
-                {/*    {...register("url", {required: true})}*/}
-                {/*    type={"url"}*/}
-                {/*    placeholder={"Url"}*/}
-                {/*    className={"w-1/2 input"}*/}
-                {/*/><br/> <br/>*/}
-                <br/>
                 <input className={"btn bg-gold-seco hover:bg-gold"} type={"submit"}/>
             </form>
         </div>
