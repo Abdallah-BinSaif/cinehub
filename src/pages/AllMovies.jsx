aeb19f3173841eaeca2ffc9d12ddbaf58729c768
Abdallah-BinSaif <binsaif.info@gmail.com>
2024-12-08 12:14:34 +0600

    Search option added

--- src/pages/AllMovies.jsx
+++ src/pages/AllMovies.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useLoaderData} from "react-router-dom";
 import MovieCard from "../components/MovieCard.jsx";
 
 const AllMovies = () => {
-    const movies = useLoaderData()
+    const fetchMovies = useLoaderData();
+    const [movies, setMovies] = useState(fetchMovies)
+    const [search, setSearch] = useState();
+    useEffect(() => {
+        fetch(`http://localhost:5000/cinemas?searchParams=${search}`).then(res=>res.json()).then(data => setMovies(data))
+    }, [search]);
+
+    // const handleSearch = (e) => {
+    //     console.log(e.target.value)
+    // }
     return (
         <div>
+
             <div className={"text-center text-5xl"}>
                 <h2>Explore Your Movie</h2>
+            </div>
+            <div className={"flex justify-center mt-8"}>
+                <label className="input input-bordered border-pri hover:border-pri flex w-1/2 items-center gap-2">
+                    <p className={"border-r border-gold pr-2 text-pri"}>Search </p>
+
+                    <input placeholder={"Popcorn ready? Let's Find a movie"} onChange={(e)=>setSearch(e.target.value)} type="text" className="w-full text-gold"/>
+                </label>
             </div>
             <div className={"container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-8 gap-3"}>
                 {
